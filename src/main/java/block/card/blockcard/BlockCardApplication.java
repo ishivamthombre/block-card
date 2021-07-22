76ea64269f095266b55b172f6ca4f5f2d8717c1e
Shivam Thombre <shivam.thombre@active.ai>
2021-07-22 16:53:58 +0530

    OK

---
+++ src/main/java/block/card/blockcard/BlockCardApplication.java
@@ -0,0 +1,13 @@
+package block.card.blockcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/block/card/blockcard/BlockCardApplicationTests.java
@@ -0,0 +1,13 @@
+package block.card.blockcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
